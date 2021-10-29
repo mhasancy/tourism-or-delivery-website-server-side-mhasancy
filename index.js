240d67d05b3044f8483c14e4eca1b213be7afcc1
mhasancy <mhasancy@gmail.com>
2021-10-29 23:02:23 +0600

    added env

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-
+//middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://Admin:MnNeX17mrsoJWsNN@cluster0.86fnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.86fnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+console.log(uri);
 
 async function run() {
   try {
@@ -46,6 +48,32 @@ async function run() {
       const result = await ordersCollection.insertOne(order);
       res.json(result);
       console.log(result);
+    });
+    //delete users orders
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
+    //update status
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const requestedStatus = req.body;
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+
+      const statusChange = {
+        $set: {
+          status: requestedStatus.status,
+        },
+      };
+      const result = await ordersCollection.updateOne(
+        filter,
+        statusChange,
+        option
+      );
+      res.json(result);
     });
   } finally {
     // Ensures that the client will close when you finish/error
