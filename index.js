27b05424bc3390b829b9c8daeff2ff4a07c850c3
mhasancy <mhasancy@gmail.com>
2021-10-30 01:07:46 +0600

    added comment

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
 //imported file
-require("dotenv").config();
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-
 //middleware
 app.use(cors());
 app.use(express.json());
 
-//database user and password
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.86fnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-//database connection
+
 async function run() {
   try {
     await client.connect();
     const database = client.db("travelNowMaster");
     const servicesCollection = database.collection("services");
     const ordersCollection = database.collection("orders");
-    //services data load to link
+
+    //data load to the services
     app.get("/services", async (req, res) => {
       const cursor = servicesCollection.find({});
       const services = await cursor.toArray();
       res.send(services);
     });
-    //orders data load to link
+
+    //data load to the orders
     app.get("/orders", async (req, res) => {
       const cursor = ordersCollection.find({});
       const orders = await cursor.toArray();
       res.send(orders);
     });
 
-    //adding services
+    //data adding to the services
     app.post("/services", async (req, res) => {
       const service = req.body;
       const result = await servicesCollection.insertOne(service);
       res.json(result);
     });
-    //adding orders
+    //data adding to the orders
     app.post("/orders", async (req, res) => {
       const order = req.body;
       const result = await ordersCollection.insertOne(order);
       res.json(result);
       console.log(result);
     });
-    //deleting orders
+    //data deleting from the orders
     app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await ordersCollection.deleteOne(query);
       res.json(result);
     });
-    //updating status
+    //data updating from  pending to approved
     app.put("/orders/:id", async (req, res) => {
       const id = req.params.id;
       const requestedStatus = req.body;
@@ -81,6 +81,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Travel Now server is running.");
+});
 
 app.listen(port, () => {
   console.log("Running on port ", port);
